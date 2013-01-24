2b0c82e170683d0034ca9f05170b2dba82067f25
Michal Hozza <mhozza@gmail.com>
2013-01-24 11:18:42 +0100

    Change filter gui

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,6 +5,11 @@
 function EmailCtrl($scope, Email, CustomerFilters) {    
     $scope.email = Email.get();
     $scope.customerFilters = CustomerFilters.query();
+
+    $scope.SetCurrentFilter = function (groupIndex, attributeIndex) {        
+        $scope.currentFilter = $scope.customerFilters[groupIndex].attributes[attributeIndex];
+        $scope.currentFilter.selectedOperator = $scope.currentFilter.operators[0];
+    };
 }
 EmailCtrl.$inject = ['$scope', 'Email', 'CustomerFilters'];
 
