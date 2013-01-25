7db00bbe91ffabb47579a8bb2c616e4a5cbf7387
Michal Hozza <mhozza@gmail.com>
2013-01-25 13:00:18 +0100

    Presuvanie filtrov

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -37,6 +37,55 @@ function EmailCtrl($scope, Email, CustomerFilters) {
       //merge lists  
       $scope.filterList.splice(index-1,2,$scope.filterList[index-1].concat($scope.filterList[index]));
     }
+
+    $scope.MoveFilter = function(groupIndex, index, direction) {
+        if(direction==1) //down
+        {
+            if(index == $scope.filterList[groupIndex].length-1)
+            {
+                if(groupIndex !=$scope.filterList.length-1)
+                {
+                    $scope.filterList[groupIndex+1].splice(0,0,$scope.filterList[groupIndex][index]);
+                    $scope.filterList[groupIndex].splice(index,1);
+                }
+            }
+            else
+            {
+                   $scope.filterList[groupIndex].splice(index+2,0,$scope.filterList[groupIndex][index]);
+                   $scope.filterList[groupIndex].splice(index,1);
+            }
+            
+        }
+        else //up
+        {
+            if(index == 0)
+            {
+                if(groupIndex != 0)
+                {
+                    $scope.filterList[groupIndex-1].splice($scope.filterList[groupIndex].length,0,$scope.filterList[groupIndex][index]);
+                    $scope.filterList[groupIndex].splice(index,1);
+                }
+            }
+            else
+            {
+                   $scope.filterList[groupIndex].splice(index-1,0,$scope.filterList[groupIndex][index]);
+                   $scope.filterList[groupIndex].splice(index+1,1);
+            }
+        }
+    };
+
+    $scope.MoveOr = function(index, direction) {        
+        if(direction==1)
+        {            
+            if($scope.filterList[index].length>0)
+                $scope.MoveFilter(index,0,-1);
+        }
+        else
+        {              
+            if($scope.filterList[index-1].length>0)
+                $scope.MoveFilter(index-1,$scope.filterList[index-1].length-1,1);
+        }
+    }
 }
 EmailCtrl.$inject = ['$scope', 'Email', 'CustomerFilters'];
 
