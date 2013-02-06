c52337e3e8423b43d0c7c0aa7b136f3593d1dc8c
Michal Hozza <mhozza@gmail.com>
2013-02-06 22:37:28 +0100

    Nove selektovatko - pomocou angular-ui a select2

--- app/js/app.js
+++ app/js/app.js
@@ -2,7 +2,7 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('marketingJS', ['myApp.filters', 'marketingjs.services', 'marketingjs.directives']).
+angular.module('marketingJS', ['myApp.filters', 'marketingjs.services', 'marketingjs.directives', 'ui']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/email', {templateUrl: 'partials/email.html', controller: EmailController});    
     $routeProvider.otherwise({redirectTo: '/email'});
