5c8940f6c721f38034ccf05cade23d57b9a9e758
Michal Hozza <mhozza@gmail.com>
2013-02-15 17:27:40 +0100

    Jednoduchy test FilterControllera

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -1,31 +1,69 @@
 'use strict';
 
 /* jasmine specs for controllers go here */
+describe('Controllers', function(){
 
-describe('MyCtrl1', function(){
-  var myCtrl1;
+  beforeEach(module('marketingjs.services'));
 
-  beforeEach(function(){
-    myCtrl1 = new MyCtrl1();
+  describe('FilterController', function(){
+    var filterController, scope, $httpBackend;
+
+    beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+      $httpBackend = _$httpBackend_;
+      $httpBackend.expectGET('filters.json').
+           respond([{"group" : "CUSTOMER", "attributes":[
+              {"id":"first-name","label":"First name","operators":["=", "LIKE"],"type":"string"},
+              {"id":"last-name","label":"Last name","operators":["=", "LIKE"],"type":"string"},
+              {"id":"email","label":"Email","operators":["=", "LIKE"],"type":"string"},
+              {"id":"phone","label":"Phone","operators":["=", "LIKE"],"type":"string"}]}]);
+      $httpBackend.expectGET('counts.json').respond({});
+      $httpBackend.expectGET('customers.json').respond([]);
+
+      scope = $rootScope.$new();
+      filterController = $controller(FilterController, {$scope: scope});
+    }));
+
+    it('should add 4 filters and or after first 3 and at the end to filter list', function() {
+      var available_attributes = [
+        {"id":"first-name","label":"First name","operators":["=", "LIKE"],"type":"string"},
+        {"id":"last-name","label":"Last name","operators":["=", "LIKE"],"type":"string"},
+        {"id":"email","label":"Email","operators":["=", "LIKE"],"type":"string"},
+        {"id":"phone","label":"Phone","operators":["=", "LIKE"],"type":"string"}
+      ];
+
+      $httpBackend.flush();
+      expect(scope.filterList.length).toBe(1);
+      expect(scope.filterList[0].length).toBe(0);
+
+      $httpBackend.expectGET('counts.json').respond({});
+      $httpBackend.expectGET('customers.json').respond([]);
+      scope.AddFilter(available_attributes[0],available_attributes[0].operators[0],"aaa");
+      $httpBackend.expectGET('counts.json').respond({});
+      $httpBackend.expectGET('customers.json').respond([]);
+      scope.AddFilter(available_attributes[1],available_attributes[0].operators[0],"bbb");
+      $httpBackend.expectGET('counts.json').respond({});
+      $httpBackend.expectGET('customers.json').respond([]);
+      scope.AddFilter(available_attributes[2],available_attributes[0].operators[0],"ccc@7segments.com");
+      $httpBackend.expectGET('counts.json').respond({});
+      $httpBackend.expectGET('customers.json').respond([]);
+      scope.AddOr();
+      $httpBackend.expectGET('counts.json').respond({});
+      $httpBackend.expectGET('customers.json').respond([]);
+      scope.AddFilter(available_attributes[3],available_attributes[0].operators[0],"0904742314");
+      $httpBackend.expectGET('counts.json').respond({});
+      $httpBackend.expectGET('customers.json').respond([]);
+      scope.AddOr();
+
+      expect(scope.filterList.length).toBe(3);
+      expect(scope.filterList[0].length).toBe(3);
+      expect(scope.filterList[1].length).toBe(1);
+      expect(scope.filterList[2].length).toBe(0);
+      expect(scope.filterList[0][0].attribute.id).toBe("first-name");
+      expect(scope.filterList[0][1].operator).toBe(available_attributes[1].operators[0]);
+      expect(scope.filterList[0][2].value).toBe("ccc@7segments.com");
+      expect(scope.filterList[1][0].attribute.id).toBe("phone");
+      expect(scope.filterList[1][0].value).toBe("0904742314");    
+    });
   });
 
-
-  it('should ....', function() {
-    //spec body
-  });
-});
-
-
-describe('MyCtrl2', function(){
-  var myCtrl2;
-
-
-  beforeEach(function(){
-    myCtrl2 = new MyCtrl2();
-  });
-
-
-  it('should ....', function() {
-    //spec body
-  });
 });
